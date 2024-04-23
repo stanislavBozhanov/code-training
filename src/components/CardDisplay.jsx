6f4020045a8eb07fb91c1a116587fce48bfc189f
MLapatova <marieta.lapatova@abv.bg>
2024-04-23 09:27:47 +0300

    Display the picture of the card

--- src/components/CardDisplay.jsx
+++ src/components/CardDisplay.jsx
@@ -1,9 +1,20 @@
-import data from '../data/data';
+import { cardData } from '../data/data';
+import { useEffect, useState } from 'react';
 
 function CardDisplay () {
-    return (
-        <div>
+const [card, setCard] = useState({})
 
+function getCard () {
+    const card = cardData.cards[0];
+    setCard(card)
+}
+useEffect(getCard, [])
+
+
+    return (
+        <div className='card'>
+            <h1>Card</h1>
+            <img src={card.image} alt="card image" />
         </div>
     )
 };
--- src/components/PartTwo.jsx
+++ src/components/PartTwo.jsx
@@ -1,9 +1,12 @@
+import CardDisplay from "./CardDisplay";
+
 function PartTwo() {
 
 
   return (
     <div>
       <h2>Part Two</h2>
+      <CardDisplay />
     </div>
   );
 }
