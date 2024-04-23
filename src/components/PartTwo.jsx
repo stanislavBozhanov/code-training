abd6b307dc67bdb507f4e436ddb2f180cbb63b7a
MLapatova <marieta.lapatova@abv.bg>
2024-04-23 20:47:20 +0300

    Task4

--- src/components/PartTwo.jsx
+++ src/components/PartTwo.jsx
@@ -1,21 +1,36 @@
 import { useEffect, useState } from 'react';
 import AllCardsDisplay from "./allCardDisplay"
 
-import { cardData } from '../data/data';
 
 function PartTwo() {
-  const [cards, setCards] = useState([])
+  const url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+  const [deckId, setDeckId] = useState('');
+  const [cards, setCards] = useState([]);
 
-  function getAllCards () {
-      const cards = cardData.cards;
-      setCards(cards)
+  const getDeckHandler = async () => {
+    const response = await fetch(url);
+    const result = await response.json();
+
+    const id = result.deck_id;
+    setDeckId(id);
   }
-  useEffect(getAllCards, [])
+
+  const fetchCardsHandler = async () => {
+    const response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`);
+    const result = await response.json();
+
+    setCards(result.cards);
+  }
 
   return (
     <div>
       <h2>Part Two</h2>
-      <AllCardsDisplay props={cards}/>
+      {cards.length
+        ? <AllCardsDisplay props={cards}/>
+        : <h3>NO CARDS FOUND</h3>
+      }
+      <button onClick={getDeckHandler}>Get Deck</button>
+      { deckId ? <button onClick={fetchCardsHandler}>Fetch cards</button> : ''}
     </div>
   );
 }
